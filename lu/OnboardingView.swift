f6c57d2cb70a3c0e0a78752bdcfd4ff790eee150
Bayan999 <b-nayf@hotmail.com>
2023-10-11 12:32:54 +0300

    comeit

---
+++ lu/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  lu
+//
+//  Created by Bayan on 26/03/1445 AH.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
---
+++ lu/OnboardingaData.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingaData.swift
+//  lu
+//
+//  Created by Bayan on 26/03/1445 AH.
+//
+
+import SwiftUI
+
+struct OnboardingaData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingaData()
+}
